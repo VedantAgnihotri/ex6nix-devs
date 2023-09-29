83364d7e80b5278d13ed99d1b1b0d0e9794d98c8
Vedant Agnihotri <84136631+VedantAgnihotri@users.noreply.github.com>
2023-09-29 14:27:35 +0530

    update

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const navLinks = document.querySelector('.nav-links')
             e.name = e.name === 'menu' ? 'close' : 'menu'
             navLinks.classList.toggle('top-[9%]')
             navLinks.classList.toggle('shadow')
-            navLinks.classList.toggle('text-cyan-600')
-            navLinks.classList.toggle('bg-gradient-to-r from-sky-700 to-sky-500 text-white')
+            navLinks.classList.toggle('text-cyan-400')
+            navLinks.classList.toggle('bg-sky-900')
         }
