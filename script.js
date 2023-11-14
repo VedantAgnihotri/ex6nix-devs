ccf07b168ce22e291dc81519452e9ed0d19d0758
rrs12 <rahilsalecha12@gmail.com>
2023-11-14 22:22:02 +0530

    update

--- script.js
+++ script.js
@@ -131,3 +131,23 @@ const observer = new IntersectionObserver(entries => {
      });
      return false;
    });
+
+
+   let sections = document.querySelectorAll('section');
+   let nav_links = document.querySelectorAll('header nav a');
+   window.onscroll = () => {
+       sections.forEach(sec => {
+           let top = window.scrollY;
+           let offset = sec.offsetTop - 150;
+           let height = sec.offsetHeight;
+           let id = sec.getAttribute('id');
+           if(top >= offset && top < offset + height) {
+               nav_links.forEach(links => {
+                   links.classList.remove('active', "text-yellow-400","dark:text-yellow-500");
+                   document.querySelector('header nav a[href*=' + id + ']').classList.add('active', "text-yellow-400","dark:text-yellow-500");                   
+               });
+           };
+       });
+   };
+
+
